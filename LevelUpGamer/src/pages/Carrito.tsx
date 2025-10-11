c79df80146070fe133265ec4e1b4beaf5fdfdc00
MarceloRauque <ma.rauque@duocuc.cl>
2025-10-11 19:04:38 -0300

    carritoClassName

--- LevelUpGamer/src/pages/Carrito.tsx
+++ LevelUpGamer/src/pages/Carrito.tsx
@@ -1,14 +1,14 @@
 export const Carrito = () => {
   return (
     <>
-    <section class="bg-azul text-light py-5">
-    <div class="container bg-dark-transparent p-4 rounded shadow-lg">
-      <h2 class="text-center text-verde orbitron text-shadow mb-4">🛒 Tu Carrito de Compras</h2>
+    <section className="bg-azul text-light py-5">
+    <div className="container bg-dark-transparent p-4 rounded shadow-lg">
+      <h2 className="text-center text-verde orbitron text-shadow mb-4">🛒 Tu Carrito de Compras</h2>
 
-      <div class="table-responsive">
-        <table class="table table-dark table-hover align-middle text-center">
+      <div className="table-responsive">
+        <table className="table table-dark table-hover align-middle text-center">
           <thead>
-            <tr class="text-verde">
+            <tr className="text-verde">
               <th>Producto</th>
               <th>Precio</th>
               <th>Cantidad</th>
@@ -17,16 +17,16 @@ export const Carrito = () => {
             </tr>
           </thead>
           <tbody id="tabla-carrito">
-            <!-- Productos generados dinámicamente -->
+            
           </tbody>
         </table>
       </div>
 
-      <div class="d-flex flex-column flex-md-row justify-content-between align-items-center mt-4">
-        <h4 id="total" class="text-verde orbitron text-shadow mb-3 mb-md-0">Total: $0</h4>
-        <div class="d-flex gap-2">
-          <button id="vaciar" class="btn btn-outline-danger">Vaciar Carrito</button>
-          <button class="btn btn-outline-success">Finalizar Compra</button>
+      <div className="d-flex flex-column flex-md-row justify-content-between align-items-center mt-4">
+        <h4 id="total" className="text-verde orbitron text-shadow mb-3 mb-md-0">Total: $0</h4>
+        <div className="d-flex gap-2">
+          <button id="vaciar" className="btn btn-outline-danger">Vaciar Carrito</button>
+          <button className="btn btn-outline-success">Finalizar Compra</button>
         </div>
       </div>
     </div>
