9553d5790b1e00e25a3fbdf48570a4dd623874e8
Alex9-hub <axelhead9@gmail.com>
2025-10-11 19:28:09 -0300

    Rutas

--- LevelUpGamer/src/App.tsx
+++ LevelUpGamer/src/App.tsx
@@ -1,15 +1,18 @@
+import { Route, Routes } from "react-router-dom"
 import { Footer } from "./components/Footer"
 import { Navbar } from "./components/Navbar"
 import { Home } from "./pages/Home"
 import { Registro } from "./pages/Registro"
+import { Products } from "./pages/Products"
 
 function App() {
 
   return (
     <>
-    <Navbar />
-    <Registro />
-    <Footer />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products" element={<Products />} />
+    </Routes>
     </>
   )
 }
